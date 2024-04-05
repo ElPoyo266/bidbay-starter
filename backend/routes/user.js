c5cf0e5d9a8f4df2e10b873940756237a85f749f
Paul Fournel <paulfournel266@gmail.com>
2024-04-05 11:21:38 +0200

    API : user finished

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,20 @@ import { User, Product, Bid } from '../orm/index.js'
 const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
-  res.status(600).send()
+  const userId = req.params.userId
+  const user = await User.findOne({
+    where: { id: userId },
+    include: [
+      { model: Product, as: 'products' },
+      { model: Bid, as: 'bids', include: [{ model: Product, as: 'product' }] }
+    ]
+  })
+  if (!user) {
+    return res.status(404).send('Error : This user does not exist')
+  } else {
+    res.status(200).json(user)
+  }
 })
+
 
 export default router
