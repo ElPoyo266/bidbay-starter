f233909f14287fddc5037795795f0b7563c1238e
Paul Fournel <paulfournel266@gmail.com>
2024-04-05 11:13:18 +0200

    API : bid finished

--- backend/routes/bid.js
+++ backend/routes/bid.js
@@ -1,16 +1,46 @@
 import authMiddleware from '../middlewares/auth.js'
-import { Bid, Product } from '../orm/index.js'
+import { Bid } from '../orm/index.js'
 import express from 'express'
-import { getDetails } from '../validators/index.js'
 
 const router = express.Router()
 
-router.delete('/api/bids/:bidId', async (req, res) => {
-  res.status(600).send()
+router.delete('/api/bids/:bidId', authMiddleware, async (req, res) => {
+  try {
+    const deleteBid = await Bid.findByPk(req.params.bidId)
+    if (!deleteBid) {
+      return res.status(404).send('Error : This product does not exist')
+    }
+    if (deleteBid.bidderId !== req.user.id && !req.user.admin) {
+      return res.status(403).send('Error : Forbidden')
+    }
+    await deleteBid.destroy()
+    res.status(204).send('Error : No Content')
+  } catch (error) {
+    return res.status(403).send('Error : Forbidden ' + error)
+  }
 })
 
-router.post('/api/products/:productId/bids', async (req, res) => {
-  res.status(600).send()
+router.post('/api/products/:productId/bids', authMiddleware, async (req, res) => {
+  try {
+    const { price } = req.body
+    if (!req.user.id && !req.user.admin) {
+      return res.status(404).send('Error : This bid does not exist')
+    }
+    let bid
+    try {
+      bid = await Bid.create({
+        productId: req.params.productId,
+        price,
+        date: Date.now(),
+        bidderId: req.user.id
+      })
+    } catch (error) {
+      return res.status(400).json({ error: 'Invalid or missing fields', details: 'Missing price field' })
+    }
+    res.status(201).send(bid)
+  } catch (error) {
+    res.status(401).send('Error : Unauthorized ' + error)
+  }
 })
 
 export default router
